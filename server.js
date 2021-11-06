c03314fa61373c2e2bf0fbf2257de9ff7518fedd
Jsummers1 <julie.summers2000@gmail.com>
2021-11-06 10:30:47 -0500

    slash

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
-app.use(express.static(__dirname, "./public"));
+app.use(express.static("./public"));
 
 const { notes } = require("./db/db");
 
@@ -60,7 +60,7 @@ app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
-app.get("*", (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
